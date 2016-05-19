15c9266c9463aa0c802f426f062f90457a95d600
Anders Olavessen <Anders Olavessen>
2016-05-19 06:25:24 -0500

    redo

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@
 // import ApplicationView from "application-view";
 import PuppyView from "puppy-view";
 
+
 export default function () {
   fetch(`http://tiny-tn.herokuapp.com/collections/ryan-puppy`)
     .then((response) => response.json())
     .then((info) => {
-      const puppyEntry = new PuppyView(info[0]);
-      document.querySelector(`.puppy-list`).appendChild(puppyEntry.element);
+      const element = document.querySelector(`.puppy-profile`);
+      const puppyProfile = new PuppyView(element, info);
     });
 }
